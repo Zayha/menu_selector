3ef5b6994f7244b6bb00a25a7385b1a2236a1cce
zayha <5350061@mail.ru>
2022-10-18 12:49:43 +0300

    add example

---
+++ examples/test.py
@@ -0,0 +1,5 @@
+#Для запуска из консоли перенести test.py в родительский каталог
+from main import menu_switcher as ms
+
+menu = ['Hello', 'Bret', 'Joza', 'Arra']
+ms(menu, position=1, name='Что-то темное:')
