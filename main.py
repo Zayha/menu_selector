7854f980bddfe3af8cbd6deb65bac9ce23698366
zayha <5350061@mail.ru>
2022-10-18 12:19:51 +0300

    add menu-name and doc

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import keyboard
 import os
 import time
+import platform
 
 
-def menu_switcher(menu, position=1):
+def menu_switcher(menu, position=1, name='Menu:'):
+    '''
+    Формирует меню без вложений, управление стрелками вверх, вниз, выбор - стрелка вправо.
 
+    :param menu: список пунктов меню
+    :param position: С какого номера будет происходить отсчет пунктов меню
+    :param name: Название меню
+    :return: индекс позиции в списке menu со сдвигом position
+    '''
+    if platform.system() == 'Windows':
+        cl = 'cls'
+        # В PowerShell
+    else:
+        cl = 'clear'
     if position < 1:
         position = 1
     if position > len(menu):
         position = len(menu)
-    os.system('cls')
-    print('Menu:')
+    os.system(cl)
+    print(name)
     for i, val in enumerate(menu, start=1):
         if i == position:
             suffix = '<--'
@@ -23,18 +36,13 @@ def menu_switcher(menu, position=1):
     match event.name:
         case "up":
             position -= 1
-            menu_switcher(menu, position)
+            menu_switcher(menu, position, name)
         case "down":
             position += 1
-            menu_switcher(menu, position)
-        case "enter":
+            menu_switcher(menu, position, name)
+        case "right":
+            # -> вместо Enter
+            os.system(cl)
             return position
         case _:
-            menu_switcher(menu, position)
-
-
-# menu = ['Скачать видеофайл со звуком', 'Выбрать дорожки самостоятельнор',
-#         'Настройка работы с файлами', 'Настройка ffmpeg']
-
-# menu_switcher(menu)
-
+            menu_switcher(menu, position, name)
